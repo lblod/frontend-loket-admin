9bd906b2a7bb1f7fda50ad333dd5129b0abeb20e
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-21 11:59:23 +0100

    Workaround bug with uri-set attributes
    uri-set attributes are currently not displayed correctly in the
    templates. Change their type to string-set so correct templates are
    generated. This must be fixed in the mu-application-generator.

--- app/models/rechtsgrond-aanstelling.js
+++ app/models/rechtsgrond-aanstelling.js
@@ -10,6 +10,6 @@ export default Model.extend({
 
   buitenwerkingtreding: attr('date'),
   inwekingtreding: attr('date'),
-  typeDocument: attr('uri-set'),
+  typeDocument: attr('string-set'),
   bekrachtigtAanstellingenVan: hasMany('mandataris', { inverse: 'rechtsgrondenAanstelling' })
 });
--- app/models/rechtsgrond-beeindiging.js
+++ app/models/rechtsgrond-beeindiging.js
@@ -10,6 +10,6 @@ export default Model.extend({
 
   buitenwerkingtreding: attr('date'),
   inwekingtreding: attr('date'),
-  typeDocument: attr('uri-set'),
+  typeDocument: attr('string-set'),
   bekrachtigtOntslagenVan: hasMany('mandataris', { inverse: 'rechtsgrondenBeeindiging' })
 });
