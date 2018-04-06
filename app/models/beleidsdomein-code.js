a0eef0469b3c3ab696986559a135eb18d61b796c
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-06 16:59:51 +0200

    adde scopeNote

--- app/models/beleidsdomein-code.js
+++ app/models/beleidsdomein-code.js
@@ -6,8 +6,9 @@ import { hasMany } from 'ember-data/relationships';
 export default Model.extend({
   // A string representation of this model, based on its attributes.
   // This is what mu-cl-resources uses to search on, and how the model will be presented while editing relationships.
-  stringRep: collect.apply(this,['id', 'label']),
+  stringRep: collect.apply(this,['id', 'label', 'scopeNote']),
 
   label: attr(),
+  scopeNote: attr(),
   mandatarissen: hasMany('mandataris', { inverse: 'beleidsdomein' })
 });
