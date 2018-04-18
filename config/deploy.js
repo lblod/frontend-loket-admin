767d5a9fd9f905bd9b7ce44553fef1f2b92cc2ef
Erika Pauwels <erika.pauwels@gmail.com>
2018-04-18 10:54:15 +0200

    Update deployment location and disable production

--- config/deploy.js
+++ config/deploy.js
@@ -12,13 +12,13 @@ module.exports = function(deployTarget) {
     'ssh-index': {
       username: 'root',
       host: 'dock.semte.ch',
-      remoteDir: '/data/digitaal-loket/dev-mdb/admin-app',
+      remoteDir: '/data/digitaal-loket/dev-loket/admin-app',
       agent: process.env.SSH_AUTH_SOCK,
       port: 2275,
       allowOverwrite: true
     },
     'rsync': {
-      dest: '/data/digitaal-loket/dev-mdb/admin-app',
+      dest: '/data/digitaal-loket/dev-loket/admin-app',
       username: 'root',
       host: 'dock.semte.ch',
       port: 2275,
@@ -29,25 +29,7 @@ module.exports = function(deployTarget) {
   };
 
   if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    ENV['ssh-index'] = {
-      username: 'root',
-      host: 'dock.semte.ch',
-      remoteDir: '/data/digitaal-loket/mdb/admin-app',
-      agent: process.env.SSH_AUTH_SOCK,
-      port: 2275,
-      allowOverwrite: true
-    };
 
-    ENV['rsync'] = {
-      dest: '/data/digitaal-loket/mdb/admin-app',
-      username: 'root',
-      host: 'dock.semte.ch',
-      port: 2275,
-      delete: false,
-      privateKey: process.env.SSH_AUTH_SOCK,
-      arg:['--verbose']
-    };
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
